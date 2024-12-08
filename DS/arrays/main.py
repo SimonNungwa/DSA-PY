e1e12ae54fa1dc6cc8dc33515c23a82ea4546b9b
SimonNungwa <simonnungwa99@gmail.com>
2024-12-08 21:06:19 +0100

    array indexing for all dimensions

--- DS/arrays/main.py
+++ DS/arrays/main.py
@@ -10,7 +10,7 @@ scores = np.array([20, 12, 24, 23, 11, 9])
 testScores = np.array([[1, 2, 4], [3, 5, 6]])
 
 # 3D array
-newTestScores =np.array([[[4, 6, 2], [6, 7, 3], [4, 2, 9]]])
+newTestScores =np.array([[[4, 6, 2], [6, 7, 3]], [[4, 2, 9], [2, 7, 9]]])
 
 # check number of dimensions in array
 scores.ndim
@@ -27,3 +27,15 @@ print(scores)
 
 
 # indexing numpy array
+# Access 1D array
+scores[2]
+
+# Access 2D array
+testScores[0, 1] # 2nd element first row
+
+# Access 3D array 
+newTestScores[0, 1, 2]
+
+# negative indexing
+scores[1, -1]  # second element second dimension
+
