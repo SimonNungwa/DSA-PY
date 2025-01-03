82bb8d5ff4f56c781102ee4bdd779bf763b4626a
Simon <simonnungwa99@gmail.com>
2025-01-03 19:45:04 +0100

    updated list count method implementation

--- DS/List/main.py
+++ DS/List/main.py
@@ -13,9 +13,9 @@ print(scoresInNum.count(scoresInNum[2]))
 
 
 # implement count 
-for idx in range(scoresInNum):
-    if idx != scoresInNum[idx + 1]:
-        idx += 1
-    else:
-        # find a way to return number of times 
-        return 
+def count_occurrences(lst, target):
+    count = 0  
+    for element in lst:  
+        if element == target:  
+            count += 1 
+    return count 
