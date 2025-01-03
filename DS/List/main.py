8c010c2e93a662df8ef93339360333c4e92f162b
Simon <simonnungwa99@gmail.com>
2025-01-03 17:50:33 +0100

    implementing python list

--- DS/List/main.py
+++ DS/List/main.py
@@ -1,4 +1,21 @@
+# implement list methods manually
+
+
 scores = [2, 4, 6, 8]
+scoresInNum = ["Two", "Four", "Six", "Eight"]
 
 # indexing
 scores[0]
+
+
+# returns number of times the arg appeared in the list
+print(scoresInNum.count(scoresInNum[2]))
+
+
+# implement count 
+for idx in range(scoresInNum):
+    if idx != scoresInNum[idx + 1]:
+        idx += 1
+    else:
+        # find a way to return number of times 
+        return 
