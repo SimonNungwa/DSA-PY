6364bce23b3cfd383a32d1c141980004b9bda7f0
Simon <simonnungwa99@gmail.com>
2025-01-15 11:32:21 +0100

    Implemented indexing

--- DS/List/main.py
+++ DS/List/main.py
@@ -8,6 +8,21 @@ scoresInNum = ["Two", "Four", "Six", "Eight"]
 scores[0]
 
 
+
+def getArrayIndex(lst, idx):
+    if 0 <= idx < len(lst):  # Check if index is valid
+        return lst[idx]      # Return the element at the index
+    else:
+        raise IndexError("Index out of range")
+
+def findIndex(lst, target):
+    for idx, element in enumerate(lst):  # Use enumerate for index tracking
+        if element == target:
+            return idx
+    return -1  # Return -1 if the target is not found
+
+
+
 # returns number of times the arg appeared in the list
 print(scoresInNum.count(scoresInNum[2]))
 
@@ -19,3 +34,4 @@ def count_occurrences(lst, target):
         if element == target:  
             count += 1 
     return count 
+
