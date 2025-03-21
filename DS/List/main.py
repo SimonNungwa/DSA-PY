52df2158f05c5e6ee1d79d9e702f623bb53a046d
Simon <simonnungwa99@gmail.com>
2025-03-21 23:42:23 +0100

    implemented list append method

--- DS/List/main.py
+++ DS/List/main.py
@@ -36,3 +36,16 @@ def count_occurrences(lst, target):
     return count 
 
 
+# implement append method
+def appendArrayElement(lst):
+    value = input("Enter value to be appended: ")  # Get user input
+    new_lst = [0] * (len(lst) + 1)  # Create a new list with one extra space
+    
+    # Copy all elements to the new list
+    for i in range(len(lst)):
+        new_lst[i] = lst[i]
+
+    # Add new value at the last index
+    new_lst[len(lst)] = value  
+
+    return new_lst  # Return the updated list
