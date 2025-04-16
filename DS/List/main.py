5f54471b74a722991f537173473b2f1ccb0ae469
Simon <simonnungwa99@gmail.com>
2025-04-16 13:10:36 +0100

    refactored main file

--- DS/List/main.py
+++ DS/List/main.py
@@ -4,27 +4,6 @@
 scores = [2, 4, 6, 8]
 scoresInNum = ["Two", "Four", "Six", "Eight"]
 
-# indexing
-scores[0]
-
-
-
-def getArrayIndex(lst, idx):
-    if 0 <= idx < len(lst):  # Check if index is valid
-        return lst[idx]      # Return the element at the index
-    else:
-        raise IndexError("Index out of range")
-
-def findIndex(lst, target):
-    for idx, element in enumerate(lst):  # Use enumerate for index tracking
-        if element == target:
-            return idx
-    return -1  # Return -1 if the target is not found
-
-
-
-# returns number of times the arg appeared in the list
-print(scoresInNum.count(scoresInNum[2]))
 
 
 # implement count 
@@ -49,3 +28,4 @@ def appendArrayElement(lst):
     new_lst[len(lst)] = value  
 
     return new_lst  # Return the updated list
+
